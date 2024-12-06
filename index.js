c157c98a95f3b4b9df06234a1acb44855c94e50e
jubayerahmmad <zubayersami510@gmail.com>
2024-12-06 09:32:10 +0600

    delete from my added visa

--- index.js
+++ index.js
@@ -54,6 +54,14 @@ async function run() {
       res.send(result);
     });
 
+    // delete visa by id
+    app.delete("/allVisas/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await visaCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // appliedUsers api
     // post
     app.post("/appliedUsers", async (req, res) => {
